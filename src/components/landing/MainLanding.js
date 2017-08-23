85dcb73b0629b935ef896ec3b89b238156926fbd
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-23 19:20:31 -0300

    Create landing feature wrapper

---
+++ src/components/landing/MainLanding.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Search from './Search';
+
+const MainLanding = () => {
+	return (
+		<div>
+			<h1>Github Client</h1>
+			<h3>Find the most popular repositories of a given user</h3>
+			<Search />
+		</div>
+	);
+};
+
+export default MainLanding;
