df8a5f6ce571cfb883e78b0ce858655789eddef5
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-29 14:29:09 -0300

    Create FETCH_REPOS action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,14 @@
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_REPOS } from './types';
 import axios from 'axios';
 
 export const fetchUser = username => async dispatch => {
 	const res = await axios.get(`https://api.github.com/users/${username}`);
 
 	dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchRepos = username => async dispatch => {
+	const res = await axios.get(`https://api.github.com/users/${username}/repos`);
+
+	dispatch({ type: FETCH_REPOS, payload: res.data });
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 
 import userReducer from './userReducer';
+import reposReducer from './reposReducer';
 
 export default combineReducers({
 	form: reduxForm,
-	user: userReducer
+	user: userReducer,
+	repos: reposReducer
 });
