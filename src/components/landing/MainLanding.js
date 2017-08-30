90ec33c42467e9d4551198f7d8d06148928e76ae
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-30 14:58:35 -0300

    Begin styling MainLanding screen

--- src/components/landing/MainLanding.js
+++ src/components/landing/MainLanding.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Search from './Search';
+
+const Wrapper = styled.div`
+	width: 100vw;
+	height: 100vh;
+	text-align: center;
+`;
+
+const Title = styled.h1`
+	color: red;
+	font-weight: 300;
+`;
+
+const SubTitle = styled.h3`color: red;`;
+
+const Card = styled.div`
+	background: #fff;
+	border-radius: 2px;
+	display: inline-block;
+	height: 300px;
+	margin: 1rem;
+	position: relative;
+	width: 300px;
+`;
 
 const MainLanding = () => {
 	return (
 		<div>
-			<h1>Github Client</h1>
-			<h3>Find the most popular repositories of a given user</h3>
-			<Search />
+			<Wrapper>
+				<Title>Github Client</Title>
+				<SubTitle>Find the most popular repositories of a given user</SubTitle>
+				<Search />
+			</Wrapper>
 		</div>
 	);
 };
--- src/components/landing/Search.js
+++ src/components/landing/Search.js
@@ -2,8 +2,52 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { fetchUser } from '../../actions';
+
+const Input = styled.input`
+	padding: 12px 20px;
+	margin: 8px 0;
+	display: inline-block;
+	border: none;
+	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+	transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+	border-radius: 4px;
+	box-sizing: border-box;
+	width: 33%;
+
+	&:hover {
+		box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+		transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+	}
+
+	&:focus {
+		outline: none;
+	}
+`;
+
+const Error = styled.div`
+	color: red;
+	font-weight: 300;
+	margin: 5px;
+`;
+
+const Submit = styled.input`
+	width: 33%;
+	background-color: #4CAF50;
+	color: white;
+	padding: 14px 20px;
+	margin: 8px 0;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+}
+
+	&:hover {
+		background-color: #45a049;
+}
+`;
 
 export class Search extends Component {
 	renderField(field) {
@@ -11,10 +55,8 @@ export class Search extends Component {
 
 		return (
 			<div>
-				<input type="text" {...input} />
-				<div>
-					{touched && error}
-				</div>
+				<Input type="text" placeholder="username..." {...input} />
+				<Error>{touched && error}</Error>
 			</div>
 		);
 	}
@@ -37,8 +79,7 @@ export class Search extends Component {
 					type="text"
 					placeholder="Search user"
 				/>
-				<button type="submit">Submit</button>
-				<div style={{ marginTop: '20px' }} />
+				<Submit type="submit" value="Submit" />
 			</form>
 		);
 	}
