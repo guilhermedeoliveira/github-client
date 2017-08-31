03f63416675ceeb7885c871663d817e4197260da
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-31 18:31:09 -0300

    Wire up MuiThemeProvider and apply injectGlobal from styled-components

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import reactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { injectGlobal } from 'styled-components';
 
 import App from './components/App';
 import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
+injectGlobal`
+  body {
+    background-color: #fafafa;
+    padding: 0;
+    margin: 0;
+    font-family: cursive;
+  }
+`;
 reactDom.render(
 	<Provider store={store}>
-		<App />
+		<MuiThemeProvider>
+			<App />
+		</MuiThemeProvider>
 	</Provider>,
 	document.querySelector('#root')
 );
